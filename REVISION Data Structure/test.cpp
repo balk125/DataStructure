83283d09a5161f1cf4e237c9695e2d4b5d7ae3df
Kanhaiya <balkrishnakanhaiya125@gmail.com>
2022-04-14 00:35:26 +0530

    revision of linkedlist

--- REVISION Data Structure/test.cpp
+++ REVISION Data Structure/test.cpp
@@ -1,42 +1,85 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
-int main()
-{
-    int first, second, lowerbound, upperbound, digitSum, temp, remainderNum, digitNum;
-    lowerbound = 1, upperbound = 1;
+#include<iostream>
 
-    cout << "Enter Frist Number: ";
-    cin >> lowerbound;
-    if (lowerbound > 100)
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+     Node *next;
+     Node(int data){
+         this->data=data;
+
+     }
+     Node(){
+
+     }
+
+};
+
+void display(Node *head){
+    Node *temp=head;
+    while (temp!=NULL)
     {
-        cout << "Please Enter more than 100 first number";
+        cout<<temp->data<<endl;
+        temp=temp->next;
     }
-    else
+    
+
+}
+void addnode(Node *head,int data){
+    Node *temp=head;
+    Node *node=new Node(data);
+    // node->data=data;
+     while (temp->next!=NULL)
     {
-        cout << "Enter Second Number: ";
-        cin >> upperbound;
-        cout << "Armstrong Numbers between " << lowerbound << " and " << upperbound << " are: ";
-        for (int num = lowerbound; num <= upperbound; num++)
-        {
-            temp = num;
-            digitNum = 0;
-            while (temp != 0)
-            {
-                digitNum++;
-                temp = temp / 10;
-            }
-            temp = num;
-            digitSum = 0;
-            while (temp != 0)
-            {
-                remainderNum = temp % 10;
-                digitSum = digitSum + pow(remainderNum, digitNum);
-                temp = temp / 10;
-            }
-            if (num == digitSum)
-                cout << num << " ";
-        }
+        
+        temp=temp->next;
     }
+    temp->next=node;
+    node->next=NULL;
+    // return head;
+
+
+}
+
+
+// 2 min rukna
+
+int main(){
+
+// Node *head=(Node *)malloc(sizeof(Node));
+Node *head=new Node[sizeof(Node)];
+Node *n1=new Node[sizeof(Node)];
+Node *n2=new Node[sizeof(Node)];
+Node *n3=new Node[sizeof(Node)];
+Node *n4=new Node[sizeof(Node)];
+
+head->data=45;
+head->next=n1;
+n1->data=100;
+n1->next=n2;
+n2->data=105;
+n2->next=n3;
+n3->data=78;
+n3->next=n4;
+n4->data=460;
+n4->next=NULL;
+addnode(head,50);
+addnode(head,5);
+addnode(head,550);
+addnode(head,550);
+addnode(head,502);
+addnode(head,501);
+addnode(head,509);
+addnode(head,506);
+addnode(head,508);
+addnode(head,502);
+addnode(head,50);
+display(head);
+
+
+
+
     return 0;
 }
